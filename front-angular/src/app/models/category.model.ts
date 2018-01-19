8117ae47cb7b6aed92ae490aa473536f5161c02b
Lucas Garin <lucas.garin73@gmail.com>
2018-01-19 17:28:42 +0100

    add all models

---
+++ front-angular/src/app/models/category.model.ts
@@ -0,0 +1,5 @@
+export class Category{
+    constructor(public CodeCat?, public LibelleCt?: string) {
+
+    }
+}
---
+++ front-angular/src/app/models/character.model.ts
@@ -0,0 +1,5 @@
+export class Character {
+    constructor(public id?, public name?: string, public address?: string, public active?: boolean) {
+
+    }
+}
