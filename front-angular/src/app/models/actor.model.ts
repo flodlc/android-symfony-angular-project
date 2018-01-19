cdd65147ed3649a73a55f498dcb541a5ba851887
Lucas Garin <lucas.garin73@gmail.com>
2018-01-19 17:28:42 +0100

    add all models

---
+++ front-angular/src/app/models/actor.model.ts
@@ -0,0 +1,6 @@
+export class Actor {
+    constructor(public NoAct?, public NomAct?: string, public PrenAct?: string,
+                public DateNaiss?: Date, public DateDeces?: Date) {
+
+    }
+}
