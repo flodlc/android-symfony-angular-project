fd793829d2557cc6fc73f9f9ffa8e126667e39c9
Lucas Garin <lucas.garin73@gmail.com>
2018-01-19 19:00:50 +0100

    add mock actors and display all

--- front-angular/src/app/actors/actors.component.ts
+++ front-angular/src/app/actors/actors.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActorService} from "../services/actor.service";
+import {Actor} from "../models/actor.model";
+import {ACTORS} from "../mock-actors";
 
 @Component({
   selector: 'app-actors',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./actors.component.css']
 })
 export class ActorsComponent implements OnInit {
+    actors: Actor[];
+    dataSearch: string = '';
 
-  constructor() { }
+    constructor(private actorServ: ActorService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        /*this.actorServ.getActors().subscribe(actors => {
+            this.actors = actors;
+        });*/
+        this.actors = ACTORS;
+    }
+
+    setFilter() {
+
+    }
 
 }
---
+++ front-angular/src/app/mock-actors.ts
@@ -0,0 +1,11 @@
+import {Actor} from "./models/actor.model";
+
+export const ACTORS: Actor[] = [
+    {NoAct:1, NomAct: 'Reno', PrenAct: 'Jean', DateNaiss: '1948-07-30'},
+    {NoAct:5, NomAct: 'Portman', PrenAct: 'Natalie', DateNaiss: '1981-06-09'},
+    {NoAct:7, NomAct: 'Dujardin', PrenAct: 'Jean', DateNaiss: '1972-06-19'},
+    {NoAct:8, NomAct: 'Bourvil', PrenAct: '',DateNaiss: '1917-07-27', DateDeces: '1970-09-23'},
+    {NoAct:12, NomAct: 'De Funes', PrenAct: 'Louis', DateNaiss: '1914-07-31', DateDeces: '1983-01-27'},
+    {NoAct:13, NomAct: 'Anglade', PrenAct: 'Jean-Hugues', DateNaiss: '1955-07-29'},
+    {NoAct:15, NomAct: 'Lambert', PrenAct: 'Christophe', DateNaiss: '1957-03-29'}
+];
