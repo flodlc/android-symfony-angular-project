d0acdc72b16bc8e476ed55bb5417f7255c036f72
Lucas Garin <lucas.garin73@gmail.com>
2018-01-22 16:06:20 +0100

    fx film

--- front-angular/src/app/film/film.component.ts
+++ front-angular/src/app/film/film.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Film} from "../models/film.model";
+import {ActivatedRoute, ParamMap} from "@angular/router";
+import {FilmService} from "../services/film.service";
 
 @Component({
   selector: 'app-film',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./film.component.css']
 })
 export class FilmComponent implements OnInit {
-
-  constructor() { }
+  film: Film;
+  constructor(private route: ActivatedRoute, private filmServ: FilmService) { }
 
   ngOnInit() {
+      this.route.paramMap
+          .switchMap((params: ParamMap) => this.filmServ.getFilm(+params.get('id')))
+          .subscribe(film => {
+            this.film = film;
+          })
   }
 
 }
